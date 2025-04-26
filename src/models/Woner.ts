2abbabf6b98f91b901efc8a22dede6acdf3fe5b5
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2025-04-26 17:32:47 +0530

    bug fixes

--- src/models/Woner.ts
+++ src/models/Woner.ts
@@ -1,35 +1,35 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IWoner extends Document {
-  cms_certification_number_ccn: string;
-  provider_name: string;
-  provider_address: string;
-  citytown: string;
-  state: string;
-  zip_code: string;
-  role_played_by_owner_or_manager_in_facility: string;
-  owner_type: string;
-  owner_name: string;
-  ownership_percentage: string;
-  association_date: string;
-  location: string;
-  processing_date: string;
+  cms_certification_number_ccn?: string;
+  provider_name?: string;
+  provider_address?: string;
+  citytown?: string;
+  state?: string;
+  zip_code?: string;
+  role_played_by_owner_or_manager_in_facility?: string;
+  owner_type?: string;
+  owner_name?: string;
+  ownership_percentage?: string;
+  association_date?: string;
+  location?: string;
+  processing_date?: string;
 }
 
 const WonerSchema: Schema = new Schema({
-  cms_certification_number_ccn: { type: String, required: true },
-  provider_name: { type: String, required: true },
-  provider_address: { type: String, required: true },
-  citytown: { type: String, required: true },
-  state: { type: String, required: true },
-  zip_code: { type: String, required: true },
-  role_played_by_owner_or_manager_in_facility: { type: String, required: true },
-  owner_type: { type: String, required: true },
-  owner_name: { type: String, required: true },
-  ownership_percentage: { type: String, required: true },
-  association_date: { type: String, required: true },
-  location: { type: String, required: true },
-  processing_date: { type: String, required: true },
+  cms_certification_number_ccn: { type: String },
+  provider_name: { type: String },
+  provider_address: { type: String },
+  citytown: { type: String },
+  state: { type: String },
+  zip_code: { type: String },
+  role_played_by_owner_or_manager_in_facility: { type: String },
+  owner_type: { type: String },
+  owner_name: { type: String },
+  ownership_percentage: { type: String },
+  association_date: { type: String },
+  location: { type: String },
+  processing_date: { type: String },
 });
 
 const Woner =
